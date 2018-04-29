b21938f4de89739c4bca0390ccfba37bc9284a6e
Maulik Desai <mdesai@esprida.com>
2018-04-28 22:14:23 -0400

    Initial data updated

--- src/main/sql/initial_data.sql
+++ src/main/sql/initial_data.sql
@@ -1,3 +1,8 @@
+INSERT INTO `tags` (`tag_name`) VALUES ('Cute Puppy'); 
+INSERT INTO `tags` (`tag_name`) VALUES ('Chubby Dog'); 
+INSERT INTO `tags` (`tag_name`) VALUES ('Cute Kitty'); 
+INSERT INTO `tags` (`tag_name`) VALUES ('Sleepy Cat'); 
+
 INSERT INTO `categories` (`category_name`) VALUES ('Dog') ON DUPLICATE KEY UPDATE category_name='Dog'; 
 INSERT INTO `categories` (`category_name`) VALUES ('Cat') ON DUPLICATE KEY UPDATE category_name='Cat'; 
 INSERT INTO `categories` (`category_name`) VALUES ('Python') ON DUPLICATE KEY UPDATE category_name='Python'; 
@@ -5,16 +10,9 @@ INSERT INTO `categories` (`category_name`) VALUES ('Python') ON DUPLICATE KEY UP
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'Charlie',category_id FROM categories WHERE category_name='Dog';
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'Max',category_id FROM categories WHERE category_name='Dog';
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'Jack',category_id FROM categories WHERE category_name='Dog';
-
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'Ashes',category_id FROM categories WHERE category_name='Cat';
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'LamNashSai',category_id FROM categories WHERE category_name='Cat';
 INSERT INTO `pets`(`pet_name`,`category_id`) SELECT 'Tiger',category_id FROM categories WHERE category_name='Cat';
-
-INSERT INTO `tags` (`tag_name`) VALUES ('Cute Puppy'); 
-INSERT INTO `tags` (`tag_name`) VALUES ('Chubby Dog'); 
-
-INSERT INTO `tags` (`tag_name`) VALUES ('Cute Kitty'); 
-INSERT INTO `tags` (`tag_name`) VALUES ('Sleepy Cat'); 
 
 INSERT INTO pettags(`tag_id`,`pet_id`) 
 	SELECT tag_id,pets.pet_id 
